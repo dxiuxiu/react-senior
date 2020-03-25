bbac913b13720fe91682eb912415bfd2d0137598
duxiuxiu <duxx@summit.com.cn>
2020-03-25 08:17:34 +0800

    chore(webpack.prd.config.js):  plugins: [

--- code/webpack.prd.config.js
+++ code/webpack.prd.config.js
@@ -1,22 +1,38 @@
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
-    mode:"production",
-    entry: {
-        index: "./src/index.jsx"
-    },
-    module:{
-        rules: [
-          {
-            test: /\.jsx$/,
-            exclude: /(node_modules)/,
-            use: {
-              loader: 'babel-loader',
-            }
-          }
-      ]
-    },
-    output: {
-      filename: 'static/js/[name].[contenthash].js',
-      // chunkFilename: 'static/js/[name].[contenthash].js',
-      chunkFilename: 'static/js/[id].[contenthash].js',
+  mode: "production",
+  entry: {
+    index: "./src/index.jsx"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  },
+  plugins: [
+    /**
+     * All files inside webpack's output.path directory will be removed once, but the
+     * directory itself will not be. If using webpack 4+'s default configuration,
+     * everything under <PROJECT_DIR>/dist/ will be removed.
+     * Use cleanOnceBeforeBuildPatterns to override this behavior.
+     *
+     * During rebuilds, all webpack assets that are not used anymore
+     * will be removed automatically.
+     *
+     * See `Options and Defaults` for information
+     */
+    new CleanWebpackPlugin(),
+  ],
+  output: {
+    filename: 'static/js/[name].[contenthash].js',
+    // chunkFilename: 'static/js/[name].[contenthash].js',
+    chunkFilename: 'static/js/[id].[contenthash].js',
   },
 }
