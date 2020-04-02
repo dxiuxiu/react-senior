227bc772732a2dc12b24c0a858ffadc76cec3edd
duxiuxiu <duxx@summit.com.cn>
2020-04-02 17:25:04 +0800

    feat(Info): 商家信息展示组件

---
+++ code/src/components/Info/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './index.less'
+
+import Star from '../../components/Start/index.jsx'
+const Info = (props) => {
+    const data = props.data
+    return (
+        <div id="detail-info-container">
+            <div className="info-container clear-fix">
+                <div className="info-img-container float-left">
+                    <img src={require(`${data.img}`)} />
+                </div>
+                <div className="info-content">
+                    <h1>{data.title}</h1>
+                    <div className="star-container">
+                        {/* 引用 Star 组件 */}
+                        <Star star={data.star} />
+                        <span className="price">￥{data.price}</span>
+                    </div>
+                    <p className="sub-title">{data.subTitle}</p>
+                </div>
+            </div>
+            {/* 设置 innerHTML - 设置的内容会被识别为 html 串*/}
+            <p dangerouslySetInnerHTML={{ __html: data.desc }} className="info-desc"></p>
+
+            {/* 设置的内容被识别为简单字符串直接展示 */}
+            {/* <p className="info-desc">
+                data.desc
+            </p> */}
+        </div>
+    )
+}
+
+export default Info
